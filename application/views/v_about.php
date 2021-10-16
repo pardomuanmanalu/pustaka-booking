0e25728603a172594ce6a42b784763c2e72bf588
pardomuanmanalu <ddomu80@gmail.com>
2021-10-16 20:20:03 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Pardomuan</li>
+        <li>Nama Belakang : Manalu</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Cengkareng, Jakarta Barat</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Singalingali</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berkuda</li>
+        <li>Berpanah</li>
+    </ul>
+</section>
